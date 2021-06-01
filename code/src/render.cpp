930f8ef06bcf1fd8cd654509937b7f8121644229
Adrián <joseadrian.guerra@enti.cat>
2021-06-01 15:26:14 +0200

    Ya no se pintan las caras internas

--- code/src/render.cpp
+++ code/src/render.cpp
@@ -256,7 +256,7 @@ void GLinit(int width, int height) {
 	glClearDepth(1.f);
 	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_DEPTH_TEST);
-	//glEnable(GL_CULL_FACE);
+	glEnable(GL_CULL_FACE);
 
 	RV::_projection = glm::perspective(glm::radians(fov), (float)width / (float)height, RV::zNear, RV::zFar * 100);
 
