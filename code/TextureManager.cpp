07a3209589767520d16a9415dae2b6f6df2d2322
Adrián <joseadrian.guerra@enti.cat>
2021-06-03 04:12:49 +0200

    TextureManager works

---
+++ code/TextureManager.cpp
@@ -0,0 +1,34 @@
+#include "TextureManager.h"
+
+TextureManager::TextureManager() {}
+TextureManager::TextureManager(const char* _texturePath, bool _fliped)
+{
+	glGenTextures(1, &img); //Cargamos en gráfica y accedemos a la variable que contendrá la textura
+	glBindTexture(GL_TEXTURE_2D, img); //Bindeamos la textura (empezamos a acceder a la información de la textura)
+
+	stbi_set_flip_vertically_on_load(_fliped); //Giramos la imagen si la vemos al reves
+	
+	data = stbi_load(_texturePath, &x, &y, &n, 4); //Cargamos la textura
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else printf("Failed to load texture\n");
+
+	//Filtros de la textura
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	//Liberamos memoria de la imagen de la textura aquí y en el cleanup
+	stbi_image_free(data);
+}
+
+const char* TextureManager::GetTexturePath()
+{
+	return texturePath;
+}
+
+unsigned int TextureManager::GetImg()
+{
+	return img;
+}
---
+++ code/TextureManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <GL/glew.h>
+#include <vector>
+#include "stb_image.h"
+
+class TextureManager
+{
+private:
+	int x = 0, y = 0, n = 0;
+	unsigned char* data = nullptr;
+	const char* texturePath = nullptr;
+	unsigned int img = 0;
+
+public:
+	TextureManager();
+	TextureManager(const char* _texturePath, bool _fliped);
+	const char* GetTexturePath();
+	unsigned int GetImg();
+};
